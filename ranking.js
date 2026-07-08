c1d93411b298766af62cbbbdbf5bf58e5e0872be
leswoo2112-cloud <leswoo2112@gmail.com>
2026-07-08 09:06:48 +0900

    ranking.js 업데이트

--- ranking.js
+++ ranking.js
@@ -29,3 +29,22 @@ function updateRanking(){
 
   rank.innerHTML = html || "아직 기록 없음";
 }
+function updateRanking() {
+    const box = document.getElementById("teamStats");
+    if (!box) return;
+
+    let list = Object.entries(players).map(([name, p]) => ({
+        name,
+        score: p.pts + p.reb + p.ast + p.stl + p.blk - p.to
+    }));
+
+    list.sort((a, b) => b.score - a.score);
+
+    let html = "<h3>🏆 선수 랭킹</h3>";
+
+    list.forEach((p, i) => {
+        html += `${i + 1}. ${p.name} (${p.score})<br>`;
+    });
+
+    box.innerHTML += "<br>" + html;
+}
